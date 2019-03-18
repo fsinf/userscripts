aad3d341cd98d10f361c5c88e55bae4b21c7991e
Lukas Anzinger <lukas@lukasanzinger.at>
2019-03-18 14:59:51 +0100

    Bump version.

--- TISS - Add VoWi LVA link.user.js
+++ TISS - Add VoWi LVA link.user.js
@@ -6,7 +6,7 @@
 // @match       https://tiss.tuwien.ac.at/course/courseDetails.xhtml*
 // @match       https://tiss.tuwien.ac.at/education/favorites.xhtml*
 // @grant       none
-// @version     1.6
+// @version     1.7
 // @downloadURL https://github.com/Lukas0907/tuwgm/raw/master/TISS%20-%20Add%20VoWi%20LVA%20link.user.js
 // @updateURL   https://github.com/Lukas0907/tuwgm/raw/master/TISS%20-%20Add%20VoWi%20LVA%20link.user.js
 // ==/UserScript==
