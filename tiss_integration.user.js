e1a56ff95fdbf4af094ae20764b38d6da8bdfebc
Lukas Anzinger <lukas@lukasanzinger.at>
2019-10-14 20:25:40 +0200

    tiss_integration.user.js: Bump version.

--- tiss_integration.user.js
+++ tiss_integration.user.js
@@ -6,7 +6,7 @@
 // @match       https://tiss.tuwien.ac.at/course/courseDetails.xhtml*
 // @match       https://tiss.tuwien.ac.at/education/favorites.xhtml*
 // @grant       none
-// @version     1.9
+// @version     1.10
 // @downloadURL https://github.com/fsinf/userscripts/raw/master/tiss_integration.user.js
 // @updateURL   https://github.com/fsinf/userscripts/raw/master/tiss_integration.user.js
 // ==/UserScript==
