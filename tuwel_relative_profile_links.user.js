6182009cd78105f7def8bd25ca31c1f5e8a606bd
Lukas Anzinger <lukas@lukasanzinger.at>
2019-11-11 11:04:24 +0100

    Add tuwel_relative_profile_links.user.js

---
+++ tuwel_relative_profile_links.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        TUWEL: Make profile links relative to course
+// @namespace   https://fsinf.at/
+// @match       https://tuwel.tuwien.ac.at/mod/forum/discuss.php
+// @grant       none
+// @version     1.0
+// ==/UserScript==
+
+var courseId = document.querySelector(".forumsearch form > input").value
+var profileLinks = document.querySelectorAll("address a").forEach(function(profileLink) {
+    profileLink.href += "&course=" + courseId;
+});
