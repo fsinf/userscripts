874541b5b5ef3bb6af8872ee529e6589b5f0f877
ben-schwen <e1225371@student.tuwien.ac.at>
2021-03-16 12:44:42 +0100

    added scale min/suggested max

--- tiss-ects-chart.user.js
+++ tiss-ects-chart.user.js
@@ -13,41 +13,41 @@
 var table = document.querySelector("#certificateList\\:j_id_3p > div > table")
 
 function findTerm(date) {
-	var splitted = date.split('.');
-	var year = parseInt(splitted[2], 10);
-	var month = parseInt(splitted[1], 10);
-	var term = (month > 2 && month < 10) ? "SS" : "WS";
-	year = (month < 3) ? year-1 : year;
-	//return term.concat(year);
-	return (year.toString()).concat(term);
+    var splitted = date.split('.');
+    var year = parseInt(splitted[2], 10);
+    var month = parseInt(splitted[1], 10);
+    var term = (month > 2 && month < 10) ? "SS" : "WS";
+    year = (month < 3) ? year-1 : year;
+    //return term.concat(year);
+    return (year.toString()).concat(term);
 }
 
 var data = [];
 for ( var i = 1; i < table.rows.length; i++ ) {
-	if (table.rows[i].cells[4].innerText == "") continue;
-	data.push({
-		'hours': table.rows[i].cells[3].innerText,
-		'ects': parseFloat(table.rows[i].cells[4].innerText),
-		'date': table.rows[i].cells[5].innerText,
-		'study': table.rows[i].cells[6].innerText,
-		'grade': table.rows[i].cells[7].innerText,
-		'term': findTerm(table.rows[i].cells[5].innerText)
-	});
+    if (table.rows[i].cells[4].innerText == "") continue;
+    data.push({
+        'hours': table.rows[i].cells[3].innerText,
+        'ects': parseFloat(table.rows[i].cells[4].innerText),
+        'date': table.rows[i].cells[5].innerText,
+        'study': table.rows[i].cells[6].innerText,
+        'grade': table.rows[i].cells[7].innerText,
+        'term': findTerm(table.rows[i].cells[5].innerText)
+    });
 }
 
 function passed(grade) {
-	var ans;
-	switch(grade) {
-		case "nicht genügend":
-			ans = false;
-			break;
-		case "unsatisfactory":
-			ans = false;
-			break;
-		default:
-			ans = true;
-	}
-	return ans;
+    var ans;
+    switch(grade) {
+        case "nicht genügend":
+            ans = false;
+            break;
+        case "unsatisfactory":
+            ans = false;
+            break;
+        default:
+            ans = true;
+    }
+    return ans;
 }
 
 var data_passed = data.filter(function(d) { return passed(d.grade) == true; })
@@ -58,12 +58,12 @@ ects_tried = [...ects_tried.entries()].sort()
 
 var ects_data = []
 for ( var i = 0; i < ects_tried.length; i++ ) {
-	var pass = ects_passed.get(ects_tried[i][0]);
-	ects_data.push({
-		'term': ects_tried[i][0],
-		'tried': ects_tried[i][1],
-		'passed': (pass == undefined) ? 0 : pass
-	});
+    var pass = ects_passed.get(ects_tried[i][0]);
+    ects_data.push({
+        'term': ects_tried[i][0],
+        'tried': ects_tried[i][1],
+        'passed': (pass == undefined) ? 0 : pass
+    });
 }
 
 
@@ -82,12 +82,19 @@ var chart = new Chart(d, {
             borderColor: 'rgb(255, 99, 132)',
             data: ects_data.map(function(d) { return d.tried })
         }, {
-        	label: 'Passed',
-        	fill: false,
-        	borderColor: 'rgb(0, 204, 102)',
-        	data: ects_data.map(function(d) { return d.passed })
+            label: 'Passed',
+            fill: false,
+            borderColor: 'rgb(0, 204, 102)',
+            data: ects_data.map(function(d) { return d.passed })
         }]
     },
 
-    options: {}
+    options: {
+        scales: {
+            y: {
+                min: 0,
+                suggestedMax: 30
+            }
+        }
+    }
 });
